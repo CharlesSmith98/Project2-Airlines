d9281c5248e7458f2b3e768b914be4a47e20a319
CharlesSmith98 <charles.w.smith@outlook.com>
2021-11-10 17:39:30 -0500

    Removed personal testing code

--- src/main/resources/webapp/arsreact/src/App.tsx
+++ src/main/resources/webapp/arsreact/src/App.tsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { TicketDisplay } from './Components/TicketDisplay';
-import { ITicket } from './Interfaces/ITicket';
 
 function App() {
 
-  let ticket: ITicket;
-  ticket = {
-    ticketId: 1,
-    flightId: 5,
-    userId: 4,
-    seatId: 1
-  }
-
   return (
     <div className="App">
-      <TicketDisplay {...ticket}/>
+      
     </div>
   );
 }
