7640f38a7a7541cf5dc7154e175784bf6ceeb2de
CharlesSmith98 <charles.w.smith@outlook.com>
2021-11-12 17:36:36 -0500

    Updated ticket portion of the API

--- src/main/java/com/project/services/TicketService.java
+++ src/main/java/com/project/services/TicketService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.project.models.Flight;
 import com.project.models.Ticket;
@@ -11,13 +12,16 @@ import com.project.models.User;
 import com.project.repository.TicketRepo;
 
 @Service
+@Transactional
 public class TicketService {
 
 	private TicketRepo tDao;
+	private FlightService fServ;
 	
 	@Autowired
-	public TicketService(TicketRepo tDao) {
+	public TicketService(TicketRepo tDao, FlightService fServ) {
 		this.tDao = tDao;
+		this.fServ = fServ;
 	}
 	
 	
@@ -71,8 +75,9 @@ public class TicketService {
 		}
 	}
 	
-	public List<Ticket> getTicketsByFlight(Flight flight) {
+	public List<Ticket> getTicketsByFlight(int flightId) {
 		try {
+			Flight flight = fServ.getFlightById(flightId);
 			List<Ticket> tickets = tDao.findAllByFlight(flight);
 			return tickets;
 		} catch (Exception e) {
