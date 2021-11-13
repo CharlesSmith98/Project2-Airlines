79ff484134706fa66545c668f7957e70ae830aad
suzannaShevchuk <79293375+suzannaShevchuk@users.noreply.github.com>
2021-11-12 17:37:40 -0800

    Update ISeat.ts

--- src/main/resources/webapp/arsreact/src/Interfaces/ISeat.ts
+++ src/main/resources/webapp/arsreact/src/Interfaces/ISeat.ts
@@ -2,17 +2,8 @@
 import { IFlight } from "./IFlight";
 
 export interface ISeat {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    seatId: number;
-    seatAvailable: boolean;
-    flight: IFlight;
-=======
-=======
-
->>>>>>> 8ba49487ce894d633520a468f5d98e741c8776f3
     seatId: number,
     flight: IFlight,
     available: boolean
->>>>>>> master
+
 }
